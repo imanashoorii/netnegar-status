5d3b8b7f1044768a5e8e9e9edeede28a0847381a
Iman Ashoori <imanashoorii.77@gmil.com>
2023-01-17 16:25:18 +0330

    .

--- netnegar/urls.py
+++ netnegar/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 import netnegar.views as views
 
 urlpatterns = [
-    path('check/set/w3k8HNgh2RQNb62BnFIp9hb2ljJh36CpaTUV4d8XW3oIQReqTiYVNGbNjOFYdSso4cbNPfJViXBYfptGCFrXP/', views.NewRecord.as_view()),
-    path('check/get/w3k8HNgh2RQNb62BnFIp9hb2ljJh36CpaTUV4d8XW3oIQReqTiYVNGbNjOFYdSso4cbNPfJViXBYfptGCFrXP/', views.GetData.as_view()),
+    path('check/set/0/', views.NewRecord.as_view()),
+    path('check/get/0/', views.GetData.as_view()),
 ]
